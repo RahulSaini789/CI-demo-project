f5f9f1ae5f7dc46c72a6e945fe232ecf578453fe
Rahul Saini <rs5778882@gmail.com>
2023-12-11 21:46:39 +0530

    enter code main.py and ci.yml

---
+++ main.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+
+fig, axx = plt.subplots()
+
+fruits = ['apple', 'blueberry', 'orange', 'cherry']
+counts = [140, 10, 30, 45]
+bar_labels = ['red', 'green', '_red', 'orange']
+bar_colors = ['tab:blue', 'tab:orange','tab:red','tab:red']
+
+ax,bar(fruits, counts, label=bar_labels, color=bar_colors)
+
+ax.set_ylabel('frult supply')
+ax.set_title('Fruit supply by kind and color')
+ax.legend(title = 'Fruit color')
+
+plt.savefig('bars.png', bbox_inches='tight')
+
+cat = ["bored", "happy", "happy","happy", "happy", "happy"]
+dog = ["bored", "bored", "bored","happy", "bored", "bored"]
+activity = ["combing", "drinking", "feeding", "nappijng", "playing", "washing"]
+
+fig, ax = plt.subplots()
+ax.plot(activity, dog, label="dog")
+ax.plot(activity, cat, label="cat")
+ax.legend()
+
+plt.savefig('lines.png', bbox_inches='tight')
+              
